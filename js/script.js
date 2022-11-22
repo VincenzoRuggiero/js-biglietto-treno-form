69239533a0492db25c47f0716420fa3b75f9ed64
VincenzoRuggiero <vincenzoru@live.it>
2022-11-22 18:35:52 +0100

    Discount calculator now works properly

--- js/script.js
+++ js/script.js
@@ -2,17 +2,19 @@ const userAge = document.getElementById("age");
 const distance = document.getElementById("distance");
 
 const kmPrice = 0.2762;
+let fullPrice;
 
 const button = document.getElementById("button");
-let fullPrice;
 
 button.addEventListener("click", function () {
   fullPrice = distance.value * kmPrice;
+  let discountSenior = (fullPrice / 100) * 33.3;
+  let discountJunior = (fullPrice / 100) * 17.5;
 
   if (parseInt(userAge.value) < 18) {
-    fullPrice = (fullPrice / 100) * 17.5;
+    fullPrice = fullPrice - discountJunior;
   } else if (parseInt(userAge.value) >= 65) {
-    fullPrice = (fullPrice / 100) * 33.3;
+    fullPrice = fullPrice - discountSenior;
   } else {
     fullPrice;
   }
